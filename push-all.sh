e6efe8a760fffc2eb70718b7e7164bb88f9944f8
Jeff Dickey <jdickey@seven-sigma.com>
2018-03-29 16:45:48 +0800

    Re-jiggering 1.1.0 after mistakenly mixing in a bunch of 1.1.1 stuff

--- push-all.sh
+++ push-all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "Will push all LOCALLY EXISTING jdickey/hanami-1.1.1-base images to Docker Hub in 5 seconds"
+echo "Will push all LOCALLY EXISTING jdickey/hanami-1.1.0-base images to Docker Hub in 5 seconds"
 echo "Hit Control-C *NOW* if this is not what you want!"
 for i in 5 4 3 2 1; do
   echo -n $i; echo -n '... '
   sleep 1
 done
-for i in `docker image ls jdickey/hanami-1.1.1-base --format='{{.ID}}\t{{.Repository}}:{{.Tag}}' | sort | cut -f 2`; do
+for i in `docker image ls jdickey/hanami-1.1.0-base --format='{{.ID}}\t{{.Repository}}:{{.Tag}}' | sort | cut -f 2`; do
   echo "Pushing $i"
   docker push $i
   echo "Done pushing $i"; echo
